e49319b908d9d68daea22906741b7b3b4b58b75c
chsu2023 <70235411+chsu2023@users.noreply.github.com>
2024-11-14 02:36:55 -0500

    Create map-reduce.sh

---
+++ map-reduce.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/jen1902/14848-project.git && \
+echo 'Ready to get in project' && \
+cd 14848-project && \
+echo 'Ready to put data into hdfs' && \
+hadoop fs -put input_data.txt / && \
+hadoop jar /usr/lib/hadoop/hadoop-streaming.jar -file mapper.py -mapper '/opt/conda/default/bin/python mapper.py' -file reducer.py -reducer '/opt/conda/default/bin/python reducer.py' -input /input_data.txt -output /output2 && \
+hadoop fs -getmerge /output2 result1 && \
+cat result1
